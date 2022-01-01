b5cb523585f97e0d1c16b90fe5197fd3ad91127a
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-01-01 14:53:43 -0600

    handlbars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,20 +2,43 @@ const { Review, User} = require('../models');
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
-    console.log(req.session);
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
 
-  res.render('homepage', {
-    id: 1,
-    post_url: 'https://handlebarsjs.com/guide/',
-    title: 'Handlebars Docs',
-    created_at: new Date(),
-    vote_count: 10,
-    comments: [{}, {}],
-    user: {
-      username: 'test_user'
-    }
-  });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/login', (req, res) => {
