b611e881a2a1a26b238f72ae6a01b96705ed8107
JoNoC0 <jnguyeningco@gmail.com>
2022-01-01 14:39:51 -0600

    added Book.js

---
+++ models/Book.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Book extends Model {}
+
+Book.init (
+    {
+    bookId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+},
+//     userId: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         references: {
+//             model: 'user',
+//             key: 'id',
+//         }
+// },
+//         reviewId: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             references: {
+//               model: 'review',
+//               key: 'id'
+//             }
+// },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+},
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'book'
+      }
+);
+
+module.exports = Book;
