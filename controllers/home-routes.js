0c51e65e0a44a82e25bc7e66ac2d31be038fa116
Diane Tritchler <dtritchler92@gmail.com>
2022-01-02 13:14:00 -0600

    finished Review button

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,50 +2,6 @@ const { Review, User, Book} = require('../models');
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
-// get all posts for homepage
-// router.get('/', (req, res) => {
-//   console.log('======================');  //For Testing
-//   Review.findAll({
-//     attributes: [
-//       'reviewId',
-//       'bookId',
-//       'userId',
-//       'comment',
-//       'created',
-//       'updated',
-//     ],
-//     include: [
-//       // {
-//       //   model: Review,
-//       //   attributes: ['reviewId', 'bookId', 'userId', 'comment', 'created', 'updated'],
-//       //   include: {
-//       //     model: User,
-//       //     attributes: ['username']
-//       //   }
-//       // },
-//       {
-//         model: User,
-//         attributes: ['username']
-//       }
-//     ]
-//   })
-    
-//   .then(dbReviewData => {
-//     const reviews = dbReviewData.map(review => review.get({ plain: true }));
-
-//     res.render('homepage', {
-//       reviews,
-//       loggedIn: req.session.loggedIn
-//     });
-//   })
-
-//   .catch(err => {
-//     console.log(err);
-//     res.status(500).json(err);
-//   });
-// });
-
-
 
 router.get('/', (req, res) => {
   console.log('======================');  //For Testing
@@ -67,6 +23,7 @@ router.get('/', (req, res) => {
       loggedIn: req.session.loggedIn
     });
   })
+  
 
   .catch(err => {
     console.log(err);
