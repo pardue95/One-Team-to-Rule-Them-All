047137e962c26ecb46d062d1e10157f5246f074f
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-01-07 18:04:48 -0600

    corrected spelling and fixed undefined error

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {maxAge:86400,},
+  cookie: {maxAge:86400},
     
   resave: false,
   saveUninitialized: true,
